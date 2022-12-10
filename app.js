f182ad5e376efd2170fcab6c83c67c2ba6705488
tzigri <tzihgri@gmail.com>
2022-12-10 10:22:28 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const port = process.env.port || 4000;
+const session = require('express-session');
+
 const app = express();
+app.use(cookieParser());
+app.use(session({
+  'secret': '343ji43j4n3jn4jk3n',
+  resave: true,
+  saveUninitialized: true
+}))
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-
-app.use('/css', express.static('css'));
-app.get('/', (request, response) => {
-    response.render('index', {
-      subject: 'Pug template engine',
-      name: 'Pug Example',
-      link: 'http://cs.uth.gr'
-    });
-  });
-app.get("")
+app.use('/', express.static('www'));
+app.get('/getSession', (req, res) => {
+  if (req.session.page_views) {
+    req.session.page_views++;
+    res.send("You visited this page " + req.session.page_views + " times");
+  } else {
+    req.session.page_views = 1;
+    res.send("Welcome to this page for the first time!");
+  }
+});
 app.listen(port, function () {
-    console.log('Server is running at ', port);
+  console.log('Server is running at ', port);
 });
